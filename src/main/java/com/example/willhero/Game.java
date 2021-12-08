670b6c03b418db8f8926a644a6c5c11c5f37ebc7
Mohit Sharma <sharmamohit78444@gmail.com>
2021-12-08 12:44:06 +0530

    Iteration 1

--- src/main/java/com/example/willhero/Game.java
+++ src/main/java/com/example/willhero/Game.java
@@ -1,21 +1,38 @@
 package com.example.willhero;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class Game extends Application {
+
+//#0098cc
+//rgba(0,152,204,204)
+public class Game extends Application{
+    static Stage greeting_stage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        this.greeting_stage = stage;
+        stage.setResizable(false);
+        Image icon = new Image("icon.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Will Hero");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("greeting_page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 934.0, 511.0,Color.rgb(29,200,255,1));
         stage.setScene(scene);
         stage.show();
+
     }
+
 
     public static void main(String[] args) {
         launch();
